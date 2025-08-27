9dfaaf2f820115b06849dcd5f74e871d308c422a
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2025-08-27 22:29:46 +0530

    Update firstprogram.sql

--- firstprogram.sql
+++ firstprogram.sql
@@ -1,10 +1,14 @@
+-- // creating first database
 create database college;
+-- syntax to use the created DB
 use college;
+-- creating table
 create table student(
 id int primary key,
 name varchar(50),
 age int not null
 );
+-- inserting accessing data
 insert into student values(1,"sunny",24);
 insert into student values(2,"srinadh",35);
 select * from student;
