b12efb18742b74c6559af36eb4917f42d9e05f47
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2025-09-03 15:50:27 +0530

    Create aggregareFunctions.sql

---
+++ aggregareFunctions.sql
@@ -0,0 +1,11 @@
+-->performs operations on a set of values and gve a single output
+ysql> select max(age) from student;
++----------+
+| max(age) |
++----------+
+|       10 |
++----------+
+--> max(),min(),avg();
+--<GROUP BY CLAUSE
+-- GROUPA ROws that group the rows with same values 
+generall  we use with aggreagre fuction 
